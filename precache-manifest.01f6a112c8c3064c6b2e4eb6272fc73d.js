ab2400050b0ea8cb9ae47bebbbec209d2402c25e
unknown <olegremnev@yandex.ru>
2020-03-11 21:52:11 +0300

    Updates

---
+++ precache-manifest.01f6a112c8c3064c6b2e4eb6272fc73d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03fd220dcbc8809e67d14764067cec3d",
+    "url": "/vk-test2/index.html"
+  },
+  {
+    "revision": "fdc632cc904b1e54b5e9",
+    "url": "/vk-test2/static/css/2.3e9c613c.chunk.css"
+  },
+  {
+    "revision": "ea16c1ca73529e2f8fb0",
+    "url": "/vk-test2/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "fdc632cc904b1e54b5e9",
+    "url": "/vk-test2/static/js/2.2a4488e4.chunk.js"
+  },
+  {
+    "revision": "ea16c1ca73529e2f8fb0",
+    "url": "/vk-test2/static/js/main.0ac46b90.chunk.js"
+  },
+  {
+    "revision": "84c1209baec7b2f03812",
+    "url": "/vk-test2/static/js/runtime-main.a84b502f.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/vk-test2/static/media/persik.4e1ec840.png"
+  }
+]);
